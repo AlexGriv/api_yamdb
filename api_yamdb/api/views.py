a4cdd141a791c6bb77bff2844f18697ca091e2f7
Александр Гривцов <lksa-wd@yandex.ru>
2022-06-08 22:13:05 +1000

    before_m

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+
+from .serializers import (CommentSerializer, ReviewSerializer)
+from reviews.models import Review, Title
+from .permissions import HasAdminRole, HasModeratorRole, IsOwner
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsOwner, HasAdminRole, HasModeratorRole)
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsOwner, HasAdminRole, HasModeratorRole)
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id, title=title_id)
+        serializer.save(author=self.request.user, review=review)
